a09c1030c8bd7fab00e635d3c8260b16a29d27f7
Melodi Demirag <melodidemirag@gmail.com>
2026-03-01 17:21:49 +0300

    Add env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDjAiJ7_q-1X31nh-O-UEd_K0d2oOq3rcc",
- authDomain: "pectus-pro.firebaseapp.com",
- projectId: "pectus-pro",
- storageBucket: "pectus-pro.firebasestorage.app",
- messagingSenderId: "697886143363",
- appId: "1:697886143363:web:49ef716a9e23e28af8fcb6"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+ authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+ projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+ storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+ messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+ appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
